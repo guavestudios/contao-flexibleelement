bab98783e42b087ba2695945a2674598526b2d0b
Rodrigo Stadler <51399652+gsrodrigostadler@users.noreply.github.com>
2019-06-19 21:27:15 +0200

    remove base template

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -2,11 +2,11 @@
 
 $GLOBALS['TL_CTE']['text']['flexibleelement'] = 'Guave\\Flexibleelement\\Elements\\ContentFlexibleElement';
 
-$GLOBALS['TL_FLEXIBLEELEMENT']['templates'] = array(
-	array(
-		'id'        => 'introtextwhite',
-		'template'  => 'content-elements/ce_intro_white'
-	),
-);
+#$GLOBALS['TL_FLEXIBLEELEMENT']['templates'] = array(
+#	array(
+#		'id'        => 'introtextwhite',
+#		'template'  => 'content-elements/ce_intro_white'
+#	),
+#);
 $GLOBALS['TL_FLEXIBLEELEMENT']['iconpath'] = 'web/assets/contentelements';
 $GLOBALS['TL_FLEXIBLEELEMENT']['iconext'] = '.jpg';
