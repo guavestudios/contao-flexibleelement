5d2a64e956071c3c6fab425dc400e48228f6601f
Rodrigo Stadler <rodrigo.stadler@guavestudios.com>
2019-11-14 10:53:42 +0100

    add images and templates

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -1,12 +1,21 @@
 <?php
 
-$GLOBALS['TL_CTE']['text']['flexibleelement'] = 'Guave\\Flexibleelement\\Elements\\ContentFlexibleElement';
+$GLOBALS['TL_CTE']['text']['flexibleelement'] = Guave\Flexibleelement\Elements\ContentFlexibleElement::class;
 
-#$GLOBALS['TL_FLEXIBLEELEMENT']['templates'] = array(
-#	array(
-#		'id'        => 'introtextwhite',
-#		'template'  => 'content-elements/ce_intro_white'
-#	),
-#);
-$GLOBALS['TL_FLEXIBLEELEMENT']['iconpath'] = 'web/assets/contentelements';
+$GLOBALS['TL_FLEXIBLEELEMENT']['templates'] = [
+    [
+        'id'       => 'flexible-title-title-text-text',
+        'template' => 'ce_2column-text',
+    ],
+    [
+        'id'       => 'flexible-right-image-text',
+        'template' => 'ce_2col-txt-img',
+    ],
+    [
+        'id'       => 'flexible-left-image-text',
+        'template' => 'ce_2col-img-txt',
+    ],
+];
+
+$GLOBALS['TL_FLEXIBLEELEMENT']['iconpath'] = 'web/bundles/guaveflexibleelement/assets';
 $GLOBALS['TL_FLEXIBLEELEMENT']['iconext'] = '.jpg';
