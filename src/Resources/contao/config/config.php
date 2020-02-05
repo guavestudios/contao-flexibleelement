fc5cd4da8a246eb874e905c5e78f46c48f4321c5
Rodrigo Stadler <51399652+gsrodrigostadler@users.noreply.github.com>
2020-02-05 10:45:23 +0100

    remove standard template and image path
    possible problem with contao 4.8

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -2,7 +2,7 @@
 
 $GLOBALS['TL_CTE']['text']['flexibleelement'] = Guave\Flexibleelement\Elements\ContentFlexibleElement::class;
 
-$GLOBALS['TL_FLEXIBLEELEMENT']['templates'] = [
+/*$GLOBALS['TL_FLEXIBLEELEMENT']['templates'] = [
     [
         'id'       => 'flexible-title-text-text',
         'template' => 'ce_2column-text',
@@ -15,7 +15,7 @@ $GLOBALS['TL_FLEXIBLEELEMENT']['templates'] = [
         'id'       => 'flexible-left-image-text',
         'template' => 'ce_2col-img-txt',
     ],
-];
+];*/
 
-$GLOBALS['TL_FLEXIBLEELEMENT']['iconpath'] = 'web/bundles/flexibleelement/assets';
+/*$GLOBALS['TL_FLEXIBLEELEMENT']['iconpath'] = 'web/bundles/flexibleelement/assets';*/
 $GLOBALS['TL_FLEXIBLEELEMENT']['iconext'] = '.jpg';
